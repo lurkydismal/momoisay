f008a85b54c209113643d32328315bea7b158306
LurkyDismal <lurkydismal@duck.com>
2026-05-04 17:36:19 +0000

    chore(momoisay): start refactoring

--- momoisay/include/momoisay.hpp
+++ momoisay/include/momoisay.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
-#include <string_view>
-
 #include "arhodigp.hpp"
 
 namespace momoisay {
 
-void printBuildType();
+auto setStatic( [[maybe_unused]] int _key,
+                [[maybe_unused]] std::string_view _value,
+                [[maybe_unused]] arhodigp::state_t _state ) -> bool;
 
-auto setVersion( [[maybe_unused]] int _key,
-                 [[maybe_unused]] std::string_view _value,
-                 [[maybe_unused]] arhodigp::state_t _state ) -> bool;
+auto setAnimated( [[maybe_unused]] int _key,
+                  [[maybe_unused]] std::string_view _value,
+                  [[maybe_unused]] arhodigp::state_t _state ) -> bool;
 
 auto run( int _argumentCount, char** _argumentVector ) -> int;
 

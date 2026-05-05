8dd627815214c6500cc5822f58865c864243f905
LurkyDismal <lurkydismal@duck.com>
2026-05-05 06:56:01 +0000

    feat(momoisay): oneshot() default argument _text to ""

--- momoisay/include/momoisay.hpp
+++ momoisay/include/momoisay.hpp
@@ -12,6 +12,6 @@ auto setAnimated( [[maybe_unused]] int _key,
                   [[maybe_unused]] std::string_view _value,
                   [[maybe_unused]] arhodigp::state_t _state ) -> bool;
 
-void oneshot();
+void oneshot( std::string_view _text = "" );
 
 } // namespace momoisay
